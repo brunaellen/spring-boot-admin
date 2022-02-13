9b9d5e6a2437053b44911a245035445f62ffaaca
brunaellen <brunaellen.souza@hotmail.com>
2022-02-13 20:39:00 +0000

    adding spring boot admin project

---
+++ src/main/java/br/com/alura/SpringBootAdminApplication.java
@@ -0,0 +1,11 @@
+package br.com.alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminApplication {
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(SpringBootAdminApplication.class, args);
+  }
+}
